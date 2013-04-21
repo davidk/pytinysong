4cb7def88c0048e4a126269c468071665d934073
davidk <kdavid@gmail.com>
2013-04-21 15:37:56 -0700

    test_request.py: Decouple tests from API results

--- tests/test_request.py
+++ tests/test_request.py
@@ -4,20 +4,23 @@
 '''
 
 from pytinysong.request import TinySongRequest
-from nose.tools import eq_, ok_
+from nose.tools import ok_
 from private_data import KEY
+
 
 def test_request_link():
     song = TinySongRequest(api_key=KEY)
     data = song.request_link('Zero 7 - Destiny')
-    eq_('http://tinysong.com/LQ5q',data)
+    ok_(data.startswith("http://tinysong.com"))
+
 
 def test_request_metadata():
     song = TinySongRequest(api_key=KEY)
     data = song.request_metadata('Overseer - Supermoves')
-    eq_('Overseer',data.artist_name)
+    ok_("Overseer" in data.artist_name or "Supermoves" in data.song_name)
+
 
 def test_request_metadata_old_property():
     song = TinySongRequest(api_key=KEY)
     data = song.request_metadata('Zero 7 - Destiny')
-    eq_(1654,data.ArtistID)
+    ok_(data.ArtistID)
