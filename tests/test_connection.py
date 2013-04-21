b34f40c0d84e63a917c2b43118f614ec923465e2
davidk <kdavid@gmail.com>
2013-04-21 15:31:13 -0700

    test_connection.py: Decouple tests from API results

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -5,25 +5,29 @@
 
 from pytinysong.connection import TinySongConnector, TinySongConnectorError
 from pytinysong.request import TinySongRequest
-from nose.tools import eq_, raises, ok_
+from nose.tools import raises, ok_
 from private_data import KEY
+
 
 @raises(TinySongConnectorError)
 def test_make_api_path_fail():
-    song = TinySongConnector(api_key=KEY) 
-    song.build_api_path("invalid_query_type","Cake - The Distance")
+    song = TinySongConnector(api_key=KEY)
+    song.build_api_path("invalid_query_type", "Cake - The Distance")
+
 
 def test_request_link():
     song = TinySongRequest(api_key=KEY)
-    eq_('http://tinysong.com/IaZs',song.request_link('Samantha James - Maybe Tomorrow'))
+    results = song.request_link('Samantha James - Maybe Tomorrow')
+    ok_(len(results) > 0)
+
 
 def test_request_metadata():
     song = TinySongRequest(api_key=KEY)
     result = song.request_metadata('Kylie - Better Than Today (Bellatrax Remix)')
-    eq_(result.url,'http://tinysong.com/IevF')
+    ok_(len(result.url) > 0)
+
 
 def test_request_search():
     song = TinySongRequest(api_key=KEY)
     results = song.search('Overseer')
     ok_(len(results) > 0)
-    
