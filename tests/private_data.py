e8666489d308c57f1c06a5d8e13fd349959a2510
davidk <kdavid@gmail.com>
2013-04-21 21:26:52 -0700

    Add travis-ci support for tests

--- tests/private_data.py
+++ tests/private_data.py
@@ -1,3 +1,7 @@
+import os
 # Replace API_KEY with your personal API key from Tinysong if you want 
 # to run tests.
-KEY=''
+if 'TRAVIS_SECURE_ENV_VARS' in os.environ and os.environ['TRAVIS_SECURE_ENV_VARS'] == 'true':
+    KEY = os.environ['KEY']
+else:
+    KEY=''
